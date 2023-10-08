cd2ee02366896f9e89f3b9e4207b0d6e28432f2a
FatyGpe0 <fatimag1818inf@gmail.com>
2023-10-08 12:13:00 -0600

    Registro funcional e interfaces de login y registro

--- client/src/app.js
+++ client/src/app.js
@@ -5,22 +5,47 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const IndexRoutes = require('./routers/index.js');
+const {default: mongoose} = require('mongoose');
+
 
 app.set('port', process.env.PORT || 4000 ); //asigno puerto 3000
+app.set('view engine', 'ejs');
 
 //Middleware utiliza morgan
 app.use(log('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use((req, res, next)=>{
+    res.locals.mensajes = '';
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+
+
 //Rutas
 app.use('/',IndexRoutes);
 
+mongoose.connect("mongodb+srv://faty:Sx09AnoKdsJewkAY@cluster0.q3on5sh.mongodb.net/Chat?retryWrites=true&w=majority")
+.then(db=>console.log('BD conectada'))
+.catch(err=>console.log(err));
 
 app.listen(app.get('port'), () =>{
     console.log('El servidor esta funcionando en el puerto', app.get('port'));
-}
-);
+});
+
+// Motor de vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 
 //establecer sistema de vistas
-app.set('views', path.join(__dirname,'views'));
-app.set('view engine','ejs');
+//app.set('views', path.join(__dirname,'views'));
+//app.set('view engine','ejs');
--- client/src/routers/index.js
+++ client/src/routers/index.js
@@ -1,42 +1,75 @@
 const express = require('express');
 const router = express.Router();
-
 const net = require('net');
+const model = require('../models/logins.js')();
+const logins = require('../models/logins.js');
+
 //const readline = require('readline-sync');
 
 const servidor={
     port:3000,
     host: 'localhost'
+};
 
-}
-const client = net.createConnection(servidor);
-    client.on('connect',()=>{
-        console.log('conexión satisfactoria')
-     //   sendLine()
-    })
-
-    let mensaje = ''; 
-
-    client.on('data', (data) => {
-        mensaje = data.toString('utf-8');
-        console.log('Mensaje del servidor: ' + mensaje);
-    });
-
-    router.get('/', async (req, res) =>{ 
-        res.render('index.ejs', {mensaje});
-    });
-
-router.post('/enviar', async (req, res) => {
-    const datos = req.body;
-    if (datos && typeof datos.mensaje === 'string') {
-        console.log("Mensaje de: ", datos.usuario,":"," ", datos.mensaje);
-        client.write(datos.usuario + ":"+ " " + datos.mensaje);
-    } else {
-        console.error("Error: 'mensaje' no es una cadena válida");
-    }
+//ruta pra registro
+router.post('/add', async(req, res)=>{
+    const info = new logins(req.body);
+    console.log(req.body);
+    await info.save();
     res.redirect('/');
-
 });
 
+//ruta para inicio de sesion
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    
+    try {
+      // Busca un usuario con el correo electrónico proporcionado
+    const user = await Login.findOne({ mail: email });
+
+      // Si no se encuentra el usuario, redirige de nuevo al formulario de inicio de sesión
+    if (!user) {
+        return res.redirect('/login');
+    }
+
+      // Verifica la contraseña
+    if (user.psw !== password) {
+        return res.redirect('/login');
+    }
+
+      // Si las credenciales son válidas, puedes redirigir al usuario a la página de chat o realizar otras acciones
+    res.redirect('/chat');
+    } catch (error) {
+    console.error(error);
+    res.status(500).send('Error interno del servidor');
+    }
+});
+
+
+const client = net.createConnection(servidor);
+    client.on('connect', ()=>{
+        console.log('conexion satisfactiria')
+        
+    })
+
+    let mensaje ='';
+
+    client.on('data', (data)=>{
+        mensaje = data.toString('utf-8');
+        console.log('mensajes del servidor:' + mensaje)
+    });
+
+router.get('/', async (req, res)=>{
+    res.render('index.ejs', {mensaje});
+});
+
+
+router.post('/send', async(req, res)=>{
+    const datos = req.body;
+        console.log("Mensaje de: " + datos.mensaje);
+        client.write(datos.mensaje);
+        res.locals.mensaje = datos.mensaje;
+    res.redirect('/');
+});
 
 module.exports = router;
