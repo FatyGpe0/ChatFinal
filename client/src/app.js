a782758d34bfa7c1a511d4457bf7d48cf2cf9c50
FatyGpe0 <fatimag1818inf@gmail.com>
2023-10-09 21:31:06 -0600

    Proteccion de Rutas exitoso

--- client/src/app.js
+++ client/src/app.js
@@ -3,12 +3,12 @@ const app = express(); //se crea la aplicación de express
 const log = require('morgan'); // para saber los clientes conectados
 const bodyParser = require('body-parser');
 const path = require('path');
-
 const IndexRoutes = require('./routers/index.js');
 const {default: mongoose} = require('mongoose');
 
 
 app.set('port', process.env.PORT || 4000 ); //asigno puerto 3000
+app.set('view engine', 'ejs');
 
 //Middleware utiliza morgan
 app.use(log('dev'));
@@ -17,7 +17,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 //Rutas
 app.use('/',IndexRoutes);
 
-app.use((req, res, next)=>{
+// establecer sistema de vistas
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+/*app.use((req, res, next)=>{
     res.locals.mensajes = '';
     next();
 });
@@ -36,19 +40,17 @@ app.get('/login', (req, res) => {
 
 app.get('/index', (req, res) => {
     res.render('index');
-});
-
-
-mongoose.connect("mongodb+srv://faty:Sx09AnoKdsJewkAY@cluster0.q3on5sh.mongodb.net/Chat?retryWrites=true&w=majority")
-.then(db=>console.log('BD conectada'))
-.catch(err=>console.log(err));
+});*/
 
 app.listen(app.get('port'), () =>{
     console.log('El servidor esta funcionando en el puerto', app.get('port'));
 });
 
-// Motor de vistas
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoose.connect("mongodb+srv://faty:Sx09AnoKdsJewkAY@cluster0.q3on5sh.mongodb.net/Chat?retryWrites=true&w=majority")
+.then(db=>console.log('BD conectada'))
+.catch(err=>console.log(err));
+
+
+
 
 
--- client/src/routers/index.js
+++ client/src/routers/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const net = require('net');
-//const model = require('../models/logins.js')();
+const model = require('../models/logins.js')();
 const logins = require('../models/logins.js');
-//const { Console, error } = require('console');
+const { Console, error } = require('console');
+const jwt = require('jsonwebtoken');
+const { decode } = require('punycode');
+const secretKey = 'tu-clave-valida';
 
 //const readline = require('readline-sync');
 
@@ -14,6 +17,40 @@ const servidor={
 
 let mensaje ='';
 
+router.get('/index', verificarToken, async (req, res) => {
+  const token = req.query.token;
+  console.log('Ruta /chat se está ejecutando'); // Agrega registros de consola
+  res.render('index', { token, mensaje }); 
+});
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
+
+function verificarToken(req, res, next){
+  const token = req.query.token;
+
+  if(!token){
+    return res.status(403).json({mensaje: 'token no proporcionado'});
+  }
+
+  jwt.verify(token,secretKey,(err, decoded)=>{
+    if (err)  {
+      console.error(err);
+      return res.status(401).json({mensaje: 'token invalido'});
+    }
+
+    req.usuario = decoded;
+    next();
+  });
+
+};
+
+
 //ruta para registro
 router.post('/add', async(req, res)=>{
     const info = new logins(req.body);
@@ -23,6 +60,21 @@ router.post('/add', async(req, res)=>{
 });
 
 //ruta para inicio de sesion
+router.get('/', async (req, res) => {
+  try {
+     // Busca un usuario con el correo electrónico proporcionado
+  const users = await Usuarios.find();
+     // Si las credenciales son válidas, puedes redirigir al usuario a la página de chat o realizar otras acciones
+  res.render('index', {
+    users
+  });
+  } catch (error) {
+  console.error(error);
+  res.status(500).send('Error al obtener usuario');
+  }
+});
+
+
 router.post('/login', async (req, res) => {
     const { nombre, psw } = req.body;
     
@@ -31,23 +83,22 @@ router.post('/login', async (req, res) => {
     const user = await logins.findOne({ nombre: nombre });
 
       // Verificar si encuentra al usuario
-    if (!user) {
-        return res.status(500).send("Usuario Invalido");
+      if (!user || user.psw !== psw) {
+        return res.status(401).send('Nombre o contrasena incorrectos');
+      } else {  
+
     }
 
-      // Verificarr la contraseña
-    if (user.psw !== psw) {
-        return res.status(500).send("Usuario no encontrado");
-    }
-
+    const usuario = { id:1, nombre: 'Ejemplo' };
+    const token = jwt.sign(usuario, secretKey, {expiresIn: '1h' });
 
       // Si el usuario es correcto envia al chat
-    res.redirect('/chat');
+    res.redirect('/index?token=' + token);
     } catch (error) {
     console.error(error);
     res.status(500).send('Error servidor');
     }
-});
+})
 
 
 const client = net.createConnection(servidor);
@@ -57,25 +108,32 @@ const client = net.createConnection(servidor);
     })
 
     
-
     client.on('data', (data)=>{
         mensaje = data.toString('utf-8');
         console.log('mensajes del servidor:' + mensaje);
 
     });
 
-router.get('/chat', async (req, res)=>{
-    res.render('chat.ejs', {mensaje});
-
-});
 
 //holaa
 router.post('/send', async(req, res)=>{
-    const datos = req.body;
-        console.log("Mensaje de: ", datos.nombre,":"," ", datos.mensaje);
-        client.write(datos.nombre + ":"+ " " + datos.mensaje);
-        res.locals.mensaje = datos.mensaje;
-    res.redirect('/chat');
+  const datos = req.body;
+  if (datos && typeof datos.mensaje === 'string') {
+      console.log("Mensaje de: " + datos.mensaje);
+      client.write(datos.mensaje);
+  } else {
+      console.error("Error: 'mensaje' no es una cadena válida");
+  }
+
+  const token = req.query.token;
+
+  if(token){
+    res.redirect('/index?token=' + token)
+  }else{
+    res.redirect('/index');
+  }
+  
 });
+
 
 module.exports = router;
